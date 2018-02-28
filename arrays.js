cdee0b0b5f5d6384c898a5aef5e953ba7e8f2f4b
Andy <amillion3@gmail.com>
2018-02-27 21:18:10 -0600

    challenge 2 complete

--- arrays.js
+++ arrays.js
@@ -17,9 +17,33 @@ if (test2 === opposite) {
   console.log("not a palindrome");
 }
 
-// if (test2 === test2.reverse()) {
-//   console.log("palindrome");
-// }
-// else {
-//   console.log("not a palindrome");
-// }
+// example
+var animals = ["fish", "dog", "cat"];
+var animalzElement = document.getElementById("animalz");
+for (var i = 0; i < animals.length; i++){
+  // console.log("animals: " + animals[i]);
+  animalzElement.innerHTML += "<h5>" + animals[i] + "</h5>";
+}
+
+//Challenge 2
+//My solution:
+var book1 = "Catch 22";
+var book2 = "Slaughterhouse 5";
+var book3 = "Fahrenheit 451";
+
+console.log(book1.replace(/[A-z\W]/gi, ""));
+console.log(book2.replace(/[A-z\W]/gi, ""));
+console.log(book3.replace(/[A-z\W]/gi, ""));
+//Zoe's solution:
+var book = "catch 22";
+var bookSplit = book.split("");
+var realNumz = [];
+for (var j = 0; j < bookSplit.length; j++) {
+  if (bookSplit[j] * 1) {
+    console.log("booksplit: " + bookSplit[j]);
+    realNumz.push(bookSplit[j]);
+  }
+}
+console.log(realNumz.join(""));
+
+//Challenge 3
