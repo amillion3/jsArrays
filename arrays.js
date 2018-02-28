c889e4f615030a6fc1b3bab72e6e257956715f5d
Andy <amillion3@gmail.com>
2018-02-27 19:48:48 -0600

    challenge 1 complete

--- arrays.js
+++ arrays.js
@@ -1,1 +1,25 @@
 console.log("Allo");
+
+// Challenge 1
+var test1 = "cat";  //false
+var test2 = "racecar"; //true
+var test3 = "bear"; //false
+var test4 = "mom"; //true
+
+var opposite = test2.split("").reverse().join("");
+//split it (to a string) "c","a","t"
+//reverse order of letters "t","a","c"
+//join letters back together as a single string "tac"
+
+if (test2 === opposite) {
+  console.log("palindrome");
+} else {
+  console.log("not a palindrome");
+}
+
+// if (test2 === test2.reverse()) {
+//   console.log("palindrome");
+// }
+// else {
+//   console.log("not a palindrome");
+// }
